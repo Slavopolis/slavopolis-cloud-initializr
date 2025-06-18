27a5d8f635634b80c71f9f389dae33f4a61ec19d
slavopolis <slavopolis@gmail.com>
2025-06-18 20:10:51 +0800

    FEAT：破坏性重构V4（->微服务）

---
+++ document/database/01-数据库创建.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE slavopolis_auth CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE slavopolis_tenant CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE slavopolis_audit CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
