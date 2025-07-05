3cfd7c033f9df8cc243f4a1fbc263e1a3d7d874c
slavopolis <slavopolis@gmail.com>
2025-07-05 12:37:20 +0800

    FEAT：代码清理

--- document/database/01-数据库创建.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE slavopolis_auth CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE DATABASE slavopolis_tenant CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE DATABASE slavopolis_audit CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
